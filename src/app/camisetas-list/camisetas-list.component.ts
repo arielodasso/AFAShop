86932a3b316fc67281b32ff97cb21cbe61fbf1d6
arielodasso <arielodassotec@gmail.com>
2022-12-02 13:21:35 -0300

    actualizo imagenes

--- src/app/camisetas-list/camisetas-list.component.ts
+++ src/app/camisetas-list/camisetas-list.component.ts
@@ -15,37 +15,37 @@ camisetas: Camisetas[] = [
   producto: "Camiseta Titular 22",
   temporada: 2022,
   stock: 1,
-  imagen: "assets/img/camisetatitular.jpg"
+  imagen: "https://www.afashop.com.ar/ccstore/v1/images/?source=/file/v3363863698249912481/products/HB9215_FC_eCom-0.jpg&height=475&width=475"
 },
 {
   producto: "Camiseta Alternativa 22",
   temporada: 2022,
   stock: 1,
-  imagen: "assets/img/camisetaalternativa.jpg"
+  imagen: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/a37248da803e40d08f48af04010aae24_9366/Camiseta_Suplente_Seleccion_Argentina_Messi_1_Azul_IQ5463_01_laydown.jpg"
 },
 {
   producto: "Camiseta Entrenamiento",
   temporada: 2022,
   stock: 1,
-  imagen: "assets/img/entrenamiento.jpg"
+  imagen: "https://media.tycsports.com/files/2022/08/02/460026/camiseta-prematch-argentina_w416.webp"
 },
 {
   producto: "Camiseta Titular 21",
   temporada: 2021,
   stock: 1,
-  imagen: "assets/img/titular2020.jpg"
+  imagen: "https://latinafy.com/images/detailed/16/Men%C2%B4s_Selecci%C3%B3n_Argentina__Titular_Official_Soccer_Team_Shirt_Argentina_-_2020-21_Edition__Latest_Edition__1.png"
 },
 {
   producto: "Camiseta Titular 19",
   temporada: 2019,
   stock: 1,
-  imagen: "assets/img/titular2019.jpg"
+  imagen: "https://static.dafiti.com.ar/p/adidas-7310-760694-1-product.jpg"
 },
 {
   producto: "Camiseta Alternativa 19",
   temporada: 2019,
   stock: 1,
-  imagen: "assets/img/alternativa2019.jpg"
+  imagen: "https://essential.vtexassets.com/arquivos/ids/301166/266-0164_4.jpg?v=637094150887800000"
 }
 ]
 
--- src/app/indumentarias-list/indumentarias-list.component.ts
+++ src/app/indumentarias-list/indumentarias-list.component.ts
@@ -15,25 +15,25 @@ indumentarias: Indumentarias[] = [
   producto: "Buzo",
   temporada: 2022,
   stock: 5,
-  imagen: "assets/img/buzo.jpg"
+  imagen: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/57fa4f61cc13476d9f4daeb500e1b519_9366/buzo-de-entrenamiento-argentina-tiro-23.jpg"
 },
 {
   producto: "Buzo",
   temporada: 2022,
   stock: 5,
-  imagen: "assets/img/buzo2.jpg"
+  imagen: "https://media.tycsports.com/files/2022/08/11/463953/el-nuevo-buzo-de-entrenamiento-de-la-seleccion-argentina-para-qatar-2022_w416.webp"
 },
 {
   producto: "Campera",
   temporada: 2022,
   stock: 5,
-  imagen: "assets/img/campera.jpg"
+  imagen: "https://pbs.twimg.com/media/FbVNpnEXEAIt1Nc?format=jpg&name=900x900"
 },
 {
   producto: "Short",
   temporada: 2022,
   stock: 5,
-  imagen: "assets/img/short.jpg"
+  imagen: "https://www.opensports.com.ar/media/catalog/product/cache/4769e4d9f3516e60f2b4303f8e5014a8/H/B/HB9216_0.jpg"
 }
 ]
 
